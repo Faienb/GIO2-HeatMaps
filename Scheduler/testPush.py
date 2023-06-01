ac8edb0c538296e039cad2dec81e8d49248fa887
Fabien Délèze <fabien.deleze@heig-vd.ch>
2023-06-01 16:38:32 +0200

    Changes in parallele et Node

--- Scheduler/testPush.py
+++
@@ -1,6 +0,0 @@
-import zmq
-context = zmq.Context()
-sender = context.socket(zmq.PUSH) # Création du socket en mode Push
-# Connexion au serveur
-sender.connect("tcp://10.191.6.34:5560") # changer ici localhost pour l'IP de votre machine
-sender.send_string("Hello, my name is RPi 1")
