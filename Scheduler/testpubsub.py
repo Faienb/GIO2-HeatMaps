3c14345934f37784d12cd434b7a6b6bd4190c023
Fabien Délèze <fabien.deleze@heig-vd.ch>
2023-06-01 14:46:26 +0200

    changement de Node

--- Scheduler/testpubsub.py
+++ Scheduler/testpubsub.py
@@ -3,8 +3,9 @@ import time
 import pickle
 import threading
 from numpy import array, zeros
+from bruno import fusion_tuile_bruno
 
-IPscheduler = "10.191.6.34"
+IPscheduler = "10.193.24.106"
 
 RPIs = {"ecg-rpi-01":{"ip":"192.168.16.17",
                        "hostname":"ecg-rpi-01",
@@ -12,8 +13,8 @@ RPIs = {"ecg-rpi-01":{"ip":"192.168.16.17",
                       "port_send":5560,
                       "status":True,
                       "byteName":b'topica'},
-        "ecg-rpi-02":{"ip":"192.168.16.18",
-                   "hostname":"ecg-rpi-02",
+        "ecg-rpi-06":{"ip":"192.168.16.18",
+                   "hostname":"ecg-rpi-06",
                       "port_rec":5556,
                       "port_send":5561,
                       "status":True,
@@ -38,19 +39,27 @@ def Subscribe():
     socket.connect(ip)
     # socket.subscribe("ecg-rpi-01")
     socket.subscribe("topica")
-    
+
     context2 = zmq.Context()
     sender2 = context2.socket(zmq.PUSH) # Création du socket en mode Push
-    sender2.connect("tcp://10.191.6.34:{:d}".format(RPIs["ecg-rpi-01"]["port_send"])) # changer ici localhost pour l'IP de votre machine
+    sender2.connect("tcp://{:s}:{:d}".format(IPscheduler,RPIs["ecg-rpi-01"]["port_send"])) # changer ici localhost pour l'IP de votre machine
 
     t0 = time.time()
     cont = True
     while cont :
+        print("Hello")
         [topic,msg] = socket.recv_multipart()
         msg2 = pickle.loads(msg)
-        msg2["data"] = zeros(shape=(255,255))
-        print(msg2)
-        time.sleep(5.0)
+        task = msg2["task"]
+        if task == 0 :
+            msg2["data"] = zeros(shape=(255,255))
+            print(msg2)
+            #Ici vont les fonctions d'Elisa
+        else :
+            tile = fusion_tuile_bruno(msg2)
+            msg2["tuile_array"] = tile
+
+        time.sleep(1.0)
         # Connexion au serveur
         sender2.send_pyobj(msg2)
 
